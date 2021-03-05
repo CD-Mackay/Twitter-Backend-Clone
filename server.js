5211a2a6533a8c940d56a7a49e9d80838c1d3bac
CD-Mackay <connor.mackay@gmail.com>
2021-03-05 13:45:33 -0500

    setup server, install dependencies

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
+
+app.set('view engine', 'ejs');
